a9d08ad1ca8957fee644ab14e3cfe7b2aa065b75
KadP <kadp@yandex.ru>
2020-04-26 03:31:59 +0300

    Разметка адаптивной сетки

--- source/js/menu.js
+++ source/js/menu.js
@@ -1,5 +1,5 @@
 var listClose = document.querySelector(".site-list__toggle");
-var listOpen = document.querySelector(".logo__menu");
+var listBurger = document.querySelector(".logo__menu");
 var listState = document.querySelector(".main-nav__list");
 
 listClose.addEventListener("click", function (evt) {
@@ -7,7 +7,9 @@ listClose.addEventListener("click", function (evt) {
   listState.classList.add("hide");
 });
 
-listOpen.addEventListener("click", function (evt) {
+listBurger.addEventListener("click", function (evt) {
   evt.preventDefault();
-  listState.classList.remove("hide");
+  if (listState.classList.contains("hide")) {
+    listState.classList.remove("hide")
+  } else (listState.classList.add("hide"));
 });
