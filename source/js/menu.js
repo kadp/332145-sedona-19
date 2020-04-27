b021e2f11c1b9a8e2708bc3bb0e2eb50c1dc57e5
KadP <kadp@yandex.ru>
2020-04-27 22:47:13 +0300

    Исправил оформление в js, убрал пустой блок scss, добавил высоту строки placeholder в форме обратной связи

--- source/js/menu.js
+++ source/js/menu.js
@@ -1,15 +1,17 @@
-var listClose = document.querySelector(".site-list__toggle");
-var listBurger = document.querySelector(".logo__menu");
+var listCloseButton = document.querySelector(".site-list__toggle");
+var listMenuButton = document.querySelector(".logo__menu");
 var listState = document.querySelector(".main-nav__list");
 
-listClose.addEventListener("click", function (evt) {
+listCloseButton.addEventListener("click", function (evt) {
   evt.preventDefault();
   listState.classList.add("hide");
 });
 
-listBurger.addEventListener("click", function (evt) {
+listMenuButton.addEventListener("click", function (evt) {
   evt.preventDefault();
   if (listState.classList.contains("hide")) {
-    listState.classList.remove("hide")
-  } else (listState.classList.add("hide"));
+    listState.classList.remove("hide");
+  } else {
+    listState.classList.add("hide");
+  }
 });
